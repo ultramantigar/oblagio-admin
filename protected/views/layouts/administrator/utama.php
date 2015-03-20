bd43012861b6ea5805fd8f83542e22adc7afa124
unknown <reza.wikrama3@gmail.com>
2015-03-20 13:44:48 +0700

    edit footer

--- protected/views/layouts/administrator/utama.php
+++ protected/views/layouts/administrator/utama.php
@@ -62,9 +62,9 @@
       <!-- end content; -->
       <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 2.0
+          <b>Version</b> 1.0
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+         <strong>Copyright &copy; 2015-2016 <a href="http://muhamadreza.com">Oblagio Studio</a>.</strong> All rights reserved.
       </footer>
     </div><!-- ./wrapper -->
     <script type="text/javascript">
